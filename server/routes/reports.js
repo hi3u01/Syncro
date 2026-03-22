8ac48786c23c00eb8fecd614975e242427fee8a1
hieu <hieuunguyenn63@gmail.com>
2026-03-22 23:42:57 +0100

    adding auth middleware and implement it to reports

--- server/routes/reports.js
+++ server/routes/reports.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const Report = require("../models/Report");
+const { protect } = require("../middleware/auth");
 
 // POST - Create a new report
-router.post("/", async (req, res) => {
+router.post("/", protect, async (req, res) => {
   try {
-    const newReport = new Report(req.body);
+    const newReport = new Report({
+      ...req.body,
+      playerId: req.user._id,
+    });
+
     const savedReport = await newReport.save();
     res.status(201).json(savedReport);
   } catch (err) {
@@ -14,9 +19,11 @@ router.post("/", async (req, res) => {
 });
 
 // GET - Get all reports for a user
-router.get("/all", async (req, res) => {
+router.get("/all", protect, async (req, res) => {
   try {
-    const reports = await Report.find().sort({ date: -1 });
+    const reports = await Report.find({ playerId: req.user._id }).sort({
+      date: -1,
+    });
     res.json(reports);
   } catch (err) {
     res.status(500).json({ error: err.message });
