1d4104640518ed3af5c917c6577e1fb26ddc462c
hieu <hieuunguyenn63@gmail.com>
2026-04-08 00:05:29 +0200

    resolve bug in routes/reports.js

--- server/routes/reports.js
+++ server/routes/reports.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const { protect } = require("../middleware/auth");
-const { createReport, getReport } = require("../controllers/teamController");
-
-router.post("/", protect, coachOnly, createReport);
-router.get("/", protect, coachOnly, getReport);
+const { createReport, getReports } = require("../controllers/reportController");
+router.post("/", protect, createReport);
+router.get("/", protect, getReports);
 
 module.exports = router;
