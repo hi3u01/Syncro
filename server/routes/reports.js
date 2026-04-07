e8215ebdb31f75dde75e2e9921463b470796fae6
hieu <hieuunguyenn63@gmail.com>
2026-04-07 23:58:26 +0200

    moving bussiness logic of reports

--- server/routes/reports.js
+++ server/routes/reports.js
@@ -1,33 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const Report = require("../models/Report");
 const { protect } = require("../middleware/auth");
+const { createReport, getReport } = require("../controllers/teamController");
 
-// POST - Create a new report
-router.post("/", protect, async (req, res) => {
-  try {
-    const newReport = new Report({
-      ...req.body,
-      playerId: req.user._id,
-    });
-
-    const savedReport = await newReport.save();
-    res.status(201).json(savedReport);
-  } catch (err) {
-    res.status(400).json({ error: err.message });
-  }
-});
-
-// GET - Get all reports for a user
-router.get("/all", protect, async (req, res) => {
-  try {
-    const reports = await Report.find({ playerId: req.user._id }).sort({
-      date: -1,
-    });
-    res.json(reports);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-});
+router.post("/", protect, coachOnly, createReport);
+router.get("/", protect, coachOnly, getReport);
 
 module.exports = router;
